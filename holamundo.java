a9902ac517ab9a4fe069823ce26920c7f03a274c
Xavi <xaviemail@example.com>
2025-02-07 10:12:50 +0100

    Prueba de push

---
+++ holamundo.java
@@ -0,0 +1,9 @@
+//Aplicacion holamundo de ejmpl
+//
+class HolaMundoApp {
+      public static void main ( String args[])
+      {
+       System.out.println ("Hola mundo");
+       System.out.println ("Hola git!");
+      }
+}
